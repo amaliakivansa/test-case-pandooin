a1e39d36111887ae274dd99e1bef8bc46ba8504e
Amalia Kivansa Anggreansi <72345090+amaliakivansa@users.noreply.github.com>
2024-08-09 22:09:42 +0700

    feat: add section footage for homepage

---
+++ src/components/molecules/SectionFootage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const SectionFootage = (props: Props) => {
+  return (
+    <section className="bg-tertiary py-[72px] mt-[54px]">
+      <div className="px-9">
+        <h2 className="base-subtitle-1">Luxury Footage</h2>
+      </div>
+    </section>
+  );
+};
+
+export default SectionFootage;
