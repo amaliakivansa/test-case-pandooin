804dec9cb091287ea736656e833724d2f4505881
Amalia Kivansa Anggreansi <72345090+amaliakivansa@users.noreply.github.com>
2024-08-09 22:04:00 +0700

    feat: add section articles for homepage

---
+++ src/components/molecules/SectionArticle.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { useFetchArticles } from "@/services";
+import ArticleCard from "../atoms/ArticleCard";
+import { IFetchArticleResponse } from "@/types/common";
+
+const SectionArticle = () => {
+  const { data, isLoading } = useFetchArticles();
+  return (
+    <section className="mt-[54px] p-4 lg:px-44">
+      <h1 className="base-subtitle-3 text-primary">Articles</h1>
+      <p className="base-text-body-1 text-primary">Our curated writings, offering something for every reader.</p>
+      {isLoading && <p className="font-albert-sans">Loading...</p>}
+      <div className="mt-6 w-full grid grid-cols-1 lg:grid-cols-4 gap-6 items-stretch">
+        {Array.isArray(data?.data) &&
+          data?.data.map((item: IFetchArticleResponse, idx) => (
+            <ArticleCard
+              key={item.title}
+              title={item.title}
+              src={item.featured_image}
+              idx={idx}
+            />
+          ))}
+      </div>
+    </section>
+  );
+};
+
+export default SectionArticle;
